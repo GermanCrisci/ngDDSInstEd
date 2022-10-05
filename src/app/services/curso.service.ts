7d3e7b3f2716ad9748ca1b8a039b3b6402824f22
Germán Crisci <80921200+GermanCrisci@users.noreply.github.com>
2022-10-05 20:02:08 -0300

    Servicio de material y curso, todavia no funcionan, tenemos dudas

--- src/app/services/curso.service.ts
+++ src/app/services/curso.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Curso }    from '../models/curso.model';
+import { Material } from '../models/material.model';
+import { Alumno }   from '../models/alumno.model';
+import { Tema }     from '../models/tema.model';
+
+const baseUrl = 'http://localhost:4200/api/cursos';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CursoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getAll(): Observable<Curso[]> {
+    return this.http.get<Curso[]>(baseUrl);
+  }
+  get(id: any): Observable<Curso> {
+    return this.http.get<Curso>(`${baseUrl}/${id}`);
+  }
+  update(id: any, data: Curso): Observable<any> {
+	//Conversione a form data
+	const formData = new FormData();
+	formData.append('id', <string>id);
+	formData.append('tema', <Tema>data.tema);
+	formData.append('fecha_inicio', <string>data.tema);
+	formData.append('id_docente', <string>data.tema);
+	formData.append('materiales', <Material[]>data.materiales);
+	formData.append('alumnos', <Alumno[]>data.alumnos);
+    return this.http.put(`${baseUrl}/update`, formData, {responseType: 'text'});
+  }
+  delete(id: any): Observable<any> {
+    return this.http.delete(`${baseUrl}/delete/${id}`, {responseType: 'text'});
+  }
 }
--- src/app/services/material.service.ts
+++ src/app/services/material.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Curso }    from '../models/curso.model';
+import { Material } from '../models/material.model';
+
+const baseUrl = 'http://localhost:4200/api/cursos';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MaterialService {
 
-  constructor() { }
-}
+  constructor(private http: HttpClient) { }
+  getAll(): Observable<Material[]> {
+    return this.http.get<Material[]>(baseUrl);
+  }
+  get(id: any): Observable<Material> {
+    return this.http.get<Material>(`${baseUrl}/${id}`);
+  }
+  update(id: any, data: Material): Observable<any> {
+	//Conversione a form data
+	const formData = new FormData();
+	formData.append('id', <string>id);
+	formData.append('titulo', <string>data.titulo);
+	formData.append('costo', <number>data.costo);
+	formData.append('curso', <Curso>data.curso);
+    return this.http.put(`${baseUrl}/update`, formData, {responseType: 'text'});
+  }
+  delete(id: any): Observable<any> {
+    return this.http.delete(`${baseUrl}/delete/${id}`, {responseType: 'text'});
+  }
