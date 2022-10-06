576ea6767ef78064f8c146c9d14b98c319614c50
FedeTusa <fedetusa01@gmail.com>
2022-10-06 11:32:08 -0300

    intento mostrar lista de cursos

--- src/app/components/curso-details/curso-details.component.ts
+++ src/app/components/curso-details/curso-details.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Curso } from 'src/app/models/curso.model';
+import { Tema } from 'src/app/models/tema.model';
+import { CursoService } from 'src/app/services/curso.service';
 
 @Component({
   selector: 'app-curso-details',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./curso-details.component.css']
 })
 export class CursoDetailsComponent implements OnInit {
+	@Input() currentCurso: Curso = {
+    nombre: '',
+    tema: undefined,
+    fecha_inicio: '',
+    id_docente: 0
+  };
 
-  constructor() { }
+  constructor(
+	private cursoService: CursoService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
+	this.getCurso(this.route.snapshot.params["id"]);
+  }
+  
+  getCurso(id: string): void {
+    this.cursoService.get(id)
+      .subscribe({
+        next: (data) => {
+          console.log(data);
+		  this.currentCurso = data;
+        },
+        error: (e) => console.error(e)
+      });
   }
 
 }
