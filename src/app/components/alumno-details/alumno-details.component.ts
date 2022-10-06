b0b4bae38e5256cd1d185f6e18732f2966a889e7
FedeTusa <fedetusa01@gmail.com>
2022-10-06 09:59:16 -0300

    agrego metodo para eliminar alumnos

--- src/app/components/alumno-details/alumno-details.component.ts
+++ src/app/components/alumno-details/alumno-details.component.ts
@@ -4,49 +4,50 @@ import { Alumno } from 'src/app/models/alumno.model';
 import { AlumnoService } from 'src/app/services/alumno.service';
 
 @Component({
-  selector: 'app-alumno-details',
-  templateUrl: './alumno-details.component.html',
-  styleUrls: ['./alumno-details.component.css']
+	selector: 'app-alumno-details',
+	templateUrl: './alumno-details.component.html',
+	styleUrls: ['./alumno-details.component.css']
 })
 export class AlumnoDetailsComponent implements OnInit {
-  @Input() currentAlumno: Alumno = {
-    nombre: '',
-    fecha_nacimiento: ''
-  };
-  
-  constructor(
-	private alumnoService: AlumnoService,
-    private route: ActivatedRoute,
-    private router: Router) { }
-    
-  ngOnInit(): void {
-     this.getAlumno(this.route.snapshot.params["id"]);
-  }
-  
-  getAlumno(id: string): void {
-    this.alumnoService.get(id)
-      .subscribe({
-        next: (data) => {
-          console.log(data);
-		  this.currentAlumno = data;
-        },
-        error: (e) => console.error(e)
-      });
-  }
-  
-  public updateAlumno(id: string, nombre: string, fecha: string): void {
-	console.log(id)
-	console.log(nombre)
-	console.log(fecha)
-    this.currentAlumno = {id: Number(id), nombre: nombre, fecha_nacimiento: fecha};
-    console.log(this.currentAlumno)
-    this.alumnoService.update(this.currentAlumno.id, this.currentAlumno)
-      .subscribe({
-        next: (res) => {
-          console.log(res);
-          alert("Alumno actualizado!")
-        },        
-        error: (e) => console.error(e)
-      });
-  }
+	@Input() currentAlumno: Alumno = {
+		nombre: '',
+		fecha_nacimiento: ''
+	};
+
+	constructor(
+		private alumnoService: AlumnoService,
+		private route: ActivatedRoute,
+		private router: Router) { }
+
+	ngOnInit(): void {
+		this.getAlumno(this.route.snapshot.params["id"]);
+	}
+
+	getAlumno(id: string): void {
+		this.alumnoService.get(id)
+			.subscribe({
+				next: (data) => {
+					console.log(data);
+					this.currentAlumno = data;
+				},
+				error: (e) => console.error(e)
+			});
+	}
+
+	public updateAlumno(id: string, nombre: string, fecha: string): void {
+		console.log(id)
+		console.log(nombre)
+		console.log(fecha)
+		this.currentAlumno = { id: Number(id), nombre: nombre, fecha_nacimiento: fecha };
+		console.log(this.currentAlumno)
+		this.alumnoService.update(this.currentAlumno.id, this.currentAlumno)
+			.subscribe({
+				next: (res) => {
+					console.log(res);
+					alert("Alumno actualizado!")
+				},
+				error: (e) => console.error(e)
+			});
+	}
+
 }
--- src/app/components/alumno-list/alumno-list.component.ts
+++ src/app/components/alumno-list/alumno-list.component.ts
@@ -1,39 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Alumno } from 'src/app/models/alumno.model';
 import { AlumnoService } from 'src/app/services/alumno.service';
 
 @Component({
-  selector: 'app-alumnos-list',
-  templateUrl: './alumno-list.component.html',
-  styleUrls: ['./alumno-list.component.css']
+	selector: 'app-alumnos-list',
+	templateUrl: './alumno-list.component.html',
+	styleUrls: ['./alumno-list.component.css']
 })
 
+
 export class AlumnoListComponent implements OnInit {
-  alumnos?: Alumno[];
-  currentAlumno: Alumno = {};
-  currentIndex = -1;
-  nombre = '';
-  constructor(private alumnoService: AlumnoService) { }
-  
-  ngOnInit(): void {
-    this.retrieveAlumnos();
-  }
-  retrieveAlumnos(): void {
-    this.alumnoService.getAll()
-      .subscribe({
-        next: (data) => {
-          this.alumnos = data;
-          console.log(data);
-        },
-        error: (e) => console.error(e)
-      });
-  }
-  deleteAlumno(id: string): void{
-	console.log("Se eliminara el alumno con id"+id)
-  }
-  refreshList(): void {
-    this.retrieveAlumnos();
-    this.currentAlumno = {};
-    this.currentIndex = -1;
-  }
+	alumnos?: Alumno[];
+	currentAlumno: Alumno = {};
+	currentIndex = -1;
+	nombre = '';
+	constructor(
+		private alumnoService: AlumnoService,
+		private route: ActivatedRoute,
+		private router: Router) { }
+
+
+	ngOnInit(): void {
+		this.retrieveAlumnos();
+	}
+	retrieveAlumnos(): void {
+		this.alumnoService.getAll()
+			.subscribe({
+				next: (data) => {
+					this.alumnos = data;
+					console.log(data);
+				},
+				error: (e) => console.error(e)
+			});
+	}
+
+	public deleteAlumno(id: string): void {
+		console.log(id)
+		console.log(this.currentAlumno.id)
+		if (confirm("Esta seguro que desea eliminar este alumno?")) {
+			this.alumnoService.delete(this.currentAlumno.id)
+				.subscribe({
+					next: (res) => {
+						console.log(res);
+						if (res == "ok") {
+							alert("Alumno eliminado con exito")
+							this.router.navigate(['/alumnos']);
+
+						} else {
+							this.router.navigate(['/alumnos']);
+							alert("Error al eliminar alumno")
+						}
+					},
+					error: (e) => console.error(e)
+				});
+
+		} else {
+			this.router.navigate(['/alumnos']);
+		}
+
+	}
+
+	refreshList(): void {
+		this.retrieveAlumnos();
+		this.currentAlumno = {};
+		this.currentIndex = -1;
+	}
 }
